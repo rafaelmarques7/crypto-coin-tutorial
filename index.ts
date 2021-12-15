1f351335ddaf0741c59bf029746a164bcf3e4c15
rafaelmarques7 <rafaelmarques76076@gmail.com>
2021-12-15 12:04:12 +0000

    add basic Block, Chain and Wallet class

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-// import * as crypto from "crypto"
+import * as crypto from "crypto";
 
 class Transaction {
     constructor(
@@ -6,4 +6,47 @@ class Transaction {
         public to: string, 
         public ammount: number
     ) {}
+}
+
+class Block {
+    constructor(
+        public previousHash: string,
+        public transaction: Transaction,
+        public date = Date.now(),
+        // public nonce: number, 
+    ) {}
+}
+
+class Chain {
+    // there can only be one chain - singleton 
+    public static instance = new Chain()
+
+    chain: Block[]
+
+    constructor() {
+        this.chain = [
+            // Genesis block
+            new Block("", new Transaction("Genesis", "Satoshi", 100))
+        ]
+    }
+
+    addBlock() {
+        return
+    }
+}
+
+class Wallet {
+    public publicKey: string
+    public privateKey: string
+
+    constructor() {
+        const keyPair = crypto.generateKeyPairSync('rsa', {
+            modulusLength: 2048,
+            publicKeyEncoding: { type: 'spki', format: 'pem' },
+            privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
+        })
+
+        this.publicKey = keyPair.publicKey
+        this.privateKey = keyPair.privateKey
+    }
 }
